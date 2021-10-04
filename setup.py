31f34ff1a2482e3215a2b0950a90dd6e2a50716b
Randy Wahidin <63757267+Randi356@users.noreply.github.com>
2021-10-04 10:57:15 +0700

    setup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/bin/env python3
-# code by : youtube.com/theunknon
+# code by : telegram xsvshacker
 
 """
 
@@ -78,7 +78,7 @@ def merge_csv():
 def update_tool():
 	import requests as r
 	banner()
-	source = r.get("https://raw.githubusercontent.com/th3unkn0n/TeleGram-Scraper/master/.image/.version")
+	source = r.get("https://raw.githubusercontent.com/Randi356/TeleGram-Scraper/master/.image/.version")
 	if source.text == '3':
 		print(gr+'['+cy+'+'+gr+']'+cy+' alredy latest version')
 	else:
@@ -86,10 +86,10 @@ def update_tool():
 		os.system('rm *.py');time.sleep(3)
 		print(gr+'['+cy+'+'+gr+']'+cy+' getting latest files ...')
 		os.system("""
-			curl -s -O https://raw.githubusercontent.com/th3unkn0n/TeleGram-Scraper/master/add2group.py
-			curl -s -O https://raw.githubusercontent.com/th3unkn0n/TeleGram-Scraper/master/scraper.py
-			curl -s -O https://raw.githubusercontent.com/th3unkn0n/TeleGram-Scraper/master/setup.py
-			curl -s -O https://raw.githubusercontent.com/th3unkn0n/TeleGram-Scraper/master/smsbot.py
+			curl -s -O https://raw.githubusercontent.com/Randi356/TeleGram-Scraper/master/add2group.py
+			curl -s -O https://raw.githubusercontent.com/Randi356/TeleGram-Scraper/master/scraper.py
+			curl -s -O https://raw.githubusercontent.com/Randi356/TeleGram-Scraper/master/setup.py
+			curl -s -O https://raw.githubusercontent.com/Randi356/TeleGram-Scraper/master/smsbot.py
 			chmod 777 *.py
 			""");time.sleep(3)
 		print(gr+'\n['+cy+'+'+gr+']'+cy+' update compled.\n')
@@ -123,5 +123,5 @@ try:
 except IndexError:
 	print('\n'+gr+'['+re+'!'+gr+']'+cy+' no argument given : '+ sys.argv[1])
 	print(gr+'['+re+'!'+gr+']'+cy+' for help use : ')
-	print(gr+'['+re+'!'+gr+']'+cy+' https://github.com/th3unkn0n/TeleGram-Scraper#-how-to-install-and-use')
+	print(gr+'['+re+'!'+gr+']'+cy+' https://github.com/Randi356/TeleGram-Scraper#-how-to-install-and-use')
 	print(gr+'$ python3 setup.py -h'+'\n')
