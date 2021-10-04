ccc3c3968edbf11bf21b685441bdcb8494fe97c8
Randy Wahidin <63757267+Randi356@users.noreply.github.com>
2021-10-04 10:58:41 +0700

    smsbot

--- smsbot.py
+++ smsbot.py
@@ -23,7 +23,7 @@ class main():
     {re} ╩ {cy}└─┘┴─┘└─┘{re}╚═╝  ╚═╝{cy}└─┘┴└─┴ ┴┴  └─┘┴└─
 
                 version : 3.1
-    youtube.com/channel/UCnknCgg_3pVXS27ThLpw3xQ
+    https://t.me/FlashProSpeed
             """)
 
     def send_sms():
